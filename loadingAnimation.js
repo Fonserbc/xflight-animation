3a227f328b6e5710385219b47f6befc2e0a2e929
Ferran Bertomeu Castells <fonserbc@gmail.com>
2021-05-10 16:13:46 +0800

    Moving pieces, starting to work on wormhole

--- loadingAnimation.js
+++ loadingAnimation.js
@@ -6,12 +6,36 @@ import { GUI } from "./lib/dat.gui.js";
 import Stats from "./lib/stats.js";
 import easing from "./lib/easings.js";
 
-var canvas, stats, camera, scene, renderer, rocks, centerArrow, arrow, gui, guiData;
+var canvas, stats, camera, scene, renderer, rocks, arrow, gui, guiData;
 var animationTime = 0;
 var DEBUG_plane;
 
-var lerp = function (f, t, a) {
-    return f * (1 - a) + t * a;
+var pieces = [];
+
+var moveTowards = function(from, to, delta)
+{
+    if (from < to) {
+        return Math.min(to, from + delta);
+    }
+    else {
+        return Math.max(to, from - delta);
+    }
+}
+
+var computeBoundingBox = function (array)
+{
+    var box = new THREE.Box3();
+    
+    for (let i = 0; i < array.length; i += 3)
+    {
+        box.min.setX(Math.min(box.min.x, array[i]));
+        box.max.setX(Math.max(box.max.x, array[i]));
+        box.min.setY(Math.min(box.min.y, array[i + 1]));
+        box.max.setY(Math.max(box.max.y, array[i + 1]));
+        box.min.setZ(Math.min(box.min.z, array[i + 2]));
+        box.max.setZ(Math.max(box.max.z, array[i + 2]));
+    }
+    return box;
 }
 
 init();
@@ -22,15 +46,17 @@ function init() {
 
     guiData = {
         spaceshipLogoSize: 1,
-        totalAnimationTime: 5,
+        totalAnimationTime: 10,
         cameraStartY: 5,
-        cameraDistanceZ: 30,
+        cameraDistanceZ: 35,
         cameraEndY: 15,
-        cameraFOV: 50,
+        cameraFOV: 60,
         spaceshipStartY: -10,
         spaceshipEndY: 15,
         holeSize: 30,
-        DEBUG_PLANE: true,
+        piecesSpeedFriction: 0.1,
+        piecesRotationFriction: 0.03,
+        DEBUG_PLANE: false,
         DEBUG_RESIZABLE_WINDOW: false
     }
 
@@ -51,7 +77,6 @@ function init() {
     if (guiData.DEBUG_PLANE) scene.add(DEBUG_plane);
 
     // moving arrow
-
     arrow = new THREE.Object3D();
     scene.add(arrow);
 
@@ -74,7 +99,7 @@ function init() {
     bgTexture.wrapT = THREE.MirroredRepeatWrapping;
 
     const invisibleMaterial = new THREE.MeshBasicMaterial({ colorWrite: false });
-    const piecesMaterial = new THREE.MeshLambertMaterial({ envMap: bgTexture });//new THREE.MeshBasicMaterial({ color: 0x111111, map: bgTexture });
+    const piecesMaterial = new THREE.MeshBasicMaterial({ color: 0x330000, side: THREE.DoubleSide });
 
     // SVGs
 
@@ -127,17 +152,13 @@ function init() {
         function (data) {
             const paths = data.paths;
             rocks = new THREE.Group();
-            var extents = {
-                x: { min: Number.POSITIVE_INFINITY, max: Number.NEGATIVE_INFINITY },
-                y: { min: Number.POSITIVE_INFINITY, max: Number.NEGATIVE_INFINITY }
-            };
+            
+            const containerPathIt = 0;
+            // if the containerPathIt != 0, we should compute container displacement beforehand
+            var containerDisplacement = new THREE.Vector3();
 
             for (let i = 0; i < paths.length; ++i) {
                 const path = paths[i];
-                const material = new THREE.MeshBasicMaterial({
-                    color: path.color,
-                    side: THREE.DoubleSide
-                });
                 const shapes = SVGLoader.createShapes(path);
 
                 //console.log(`path ${i} has ${shapes.length} shapes`);
@@ -145,43 +166,121 @@ function init() {
                 for (let j = 0; j < shapes.length; ++j) {
                     const shape = shapes[j];
                     const geometry = new THREE.ShapeGeometry(shape);
-
-                    const positions = geometry.attributes.position;
-
-                    for (let p = 0; p < positions.array.length;) {
-                        let x = positions.array[p];
-                        let y = positions.array[p + 1];
-
-                        if (x < extents.x.min) extents.x.min = x;
-                        if (x > extents.x.max) extents.x.max = x;
-                        if (y < extents.y.min) extents.y.min = y;
-                        if (y > extents.y.max) extents.y.max = y;
-
-                        p += positions.itemSize;
-                    }
-
-                    const mesh = new THREE.Mesh(geometry, i == 0 ? invisibleMaterial : piecesMaterial);
-                    mesh.position.z = i*3;
-                    rocks.add(mesh);
-                    if (i == 0) {
+                    geometry.rotateX(Math.PI * -0.5);
+                    
+                    if (i == containerPathIt) {
+                        // Vertex corrections
+                        geometry.computeBoundingBox();
+                        //geometry.boundingBox = computeBoundingBox(geometry.attributes.position.array);
+                        containerDisplacement = new THREE.Vector3();
+                        geometry.boundingBox.getCenter(containerDisplacement);
+                        containerDisplacement.multiplyScalar(-1);
+                        
+                        geometry.translate(containerDisplacement.x, containerDisplacement.y, containerDisplacement.z);
+                        geometry.computeBoundingBox();
+                        rocks.boundingBox = geometry.boundingBox.clone();
+                        
+                        const mesh = new THREE.Mesh(geometry, invisibleMaterial);
+                        rocks.add(mesh);
+                        
                         // This is the triangle that is going to block the bottom side
                         const frontBlocker = new THREE.BufferGeometry();
                         const vertices = new Float32Array([
-                            extents.x.min, extents.y.min, 0,
-                            (extents.x.min + extents.x.max) * 0.5, extents.y.min, -1000,
-                            extents.x.max, extents.y.min, 0
+                            geometry.boundingBox.min.x, geometry.boundingBox.min.y, geometry.boundingBox.max.z,
+                            (geometry.boundingBox.min.x + geometry.boundingBox.max.x) * 0.5, geometry.boundingBox.min.y - 1000, geometry.boundingBox.max.z,
+                            geometry.boundingBox.max.x, geometry.boundingBox.min.y, geometry.boundingBox.max.z
                         ]);
                         frontBlocker.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
                         rocks.add(new THREE.Mesh(frontBlocker, invisibleMaterial));
+                        
+                        const backBlocker = new THREE.BufferGeometry();
+                        const backVertices = new Float32Array([
+                            geometry.boundingBox.min.x, geometry.boundingBox.min.y, geometry.boundingBox.min.z,
+                            (geometry.boundingBox.min.x + geometry.boundingBox.max.x) * 0.5, geometry.boundingBox.min.y - 1000, geometry.boundingBox.min.z,
+                            geometry.boundingBox.max.x, geometry.boundingBox.min.y, geometry.boundingBox.min.z
+                        ]);
+                        backBlocker.setAttribute('position', new THREE.BufferAttribute(backVertices, 3));
+                        rocks.add(new THREE.Mesh(backBlocker, new THREE.MeshBasicMaterial({color: 0x000000})));
+                    }
+                    else { // Add the rocks as pieces for movement
+                        geometry.translate(containerDisplacement.x, containerDisplacement.y, containerDisplacement.z);
+                        geometry.computeBoundingBox();
+                        var pieceDisplacement = new THREE.Vector3();
+                        geometry.boundingBox.getCenter(pieceDisplacement);
+                        
+                        const pivot = new THREE.Object3D();
+                        rocks.add(pivot);
+                        pivot.position.set(pieceDisplacement.x, pieceDisplacement.y, pieceDisplacement.z);
+                        geometry.translate(-pieceDisplacement.x, -pieceDisplacement.y, -pieceDisplacement.z);
+                        
+                        const mesh = new THREE.Mesh(geometry, false ? invisibleMaterial : piecesMaterial);
+                        // TODO assign material depending on moving
+                        pivot.add(mesh);
+                        
+                        //console.log(pivot);
+                        
+                        var vel = pieceDisplacement.clone();
+                        vel.y = 45;
+                        vel.normalize();
+                        
+                        var piece = {
+                            object3D: pivot,
+                            originalPosition: pieceDisplacement,
+                            moving: true,
+                            rotationAxis: new THREE.Vector3().random().normalize(),
+                            rotationSpeed: 0,
+                            velocity: vel,
+                            speed: 0,
+                            
+                            init: function () {
+                                this.object3D.position.copy(this.originalPosition);
+                                this.object3D.quaternion.identity();
+                                
+                                var receivedSpeed = easing.easeInCirc(Math.abs(this.velocity.y));
+                                
+                                
+                                this.rotationSpeed = receivedSpeed * (2 + THREE.MathUtils.lerp(-1, 1, Math.random()));
+                                this.speed = receivedSpeed * (50 + THREE.MathUtils.lerp(-5, 5, Math.random()));
+                            }
+                        }
+                        piece.init();
+                        pieces.push(piece);
                     }
                 }
             }
-            rocks.setRotationFromAxisAngle(new THREE.Vector3(-1, 0, 0), Math.PI * 0.5);
-            rocks.extents = extents;
+            
+            // wormhole circles and lines
+            const wormlinesMaterial = new THREE.LineBasicMaterial({
+                color: 0x00ff00
+            });
+            
+            const circleSegments = 16;
+            const numberOfCircles = 5;
+            const circleRadiusMax = (rocks.boundingBox.max.x - rocks.boundingBox.min.x) * 0.2;
+            const circleRadiusMin = circleRadiusMax * 0.3;
+            const distanceBetweenCircles = circleRadiusMax * 0.1;
+            var rocksCenter = new THREE.Vector3();
+            rocks.boundingBox.getCenter(rocksCenter);
+            
+            for(let i = 0; i < numberOfCircles; ++i)
+            {
+                const points = [];
+                const radius = THREE.MathUtils.lerp(circleRadiusMax, circleRadiusMin, easing.easeOutQuad(i / (numberOfCircles - 1)));
+                const depth = (-i - 1) * distanceBetweenCircles;
+                for (let j = 0; j < circleSegments + 1; ++j)
+                {
+                    var angle = j * Math.PI * 2 / circleSegments;
+                    points.push(new THREE.Vector3(Math.cos(angle) * radius, depth, Math.sin(angle) * radius).add(rocksCenter));
+                }
+                const wormholeGeometry = new THREE.BufferGeometry().setFromPoints( points );
+                const line = new THREE.Line( wormholeGeometry, wormlinesMaterial );
+                
+                rocks.add(line);
+            }
+            
             rocks.resizeHole = function (s) {
-                var size = s / (rocks.extents.x.max - rocks.extents.x.min);
+                var size = s / (rocks.boundingBox.max.x - rocks.boundingBox.min.x);
                 rocks.scale.set(size, size, size);
-                rocks.position.set(-(rocks.extents.x.max - rocks.extents.x.min) * 0.5 * size, 0, (rocks.extents.y.max - rocks.extents.y.min) * 0.5 * size);
 
             };
             rocks.resizeHole(guiData.holeSize);
@@ -213,7 +312,12 @@ function createDebugGUI ()
         rocks.resizeHole(guiData.holeSize);
         restart();
     });
-
+    var rocksGUI = gui.addFolder("Rocks");
+    rocksGUI.add(guiData, "piecesSpeedFriction", 0, 10).name("translation friction");
+    rocksGUI.add(guiData, "piecesRotationFriction", 0, 2).name("rotation friction");
+    // TODO rocks rotation and speed GUI
+    
+    
     var debugGUI = gui.addFolder("DEBUG");
     debugGUI.add(guiData, "DEBUG_RESIZABLE_WINDOW").name("Resizable Window");
     debugGUI.add(guiData, "DEBUG_PLANE").name("Show debug plane").onChange(function () {
@@ -266,12 +370,38 @@ function update(deltaTime) {
 
     const animationFactor = Math.max(0, Math.min(1, animationTime / guiData.totalAnimationTime));
 
-    arrow.position.y = lerp(guiData.spaceshipStartY, guiData.spaceshipEndY, easing.easeOutExpo(animationFactor));
-    camera.position.y = lerp(guiData.cameraStartY, guiData.cameraEndY, easing.easeInOutSine(animationFactor));
+    arrow.position.y = THREE.MathUtils.lerp(guiData.spaceshipStartY, guiData.spaceshipEndY, easing.easeOutExpo(animationFactor));
+    camera.position.y = THREE.MathUtils.lerp(guiData.cameraStartY, guiData.cameraEndY, easing.easeInOutSine(animationFactor));
+    
+    //camera.lookAt(arrow.position);
+    
+    for (let i = 0; i < pieces.length; ++i)
+    {
+        const p = pieces[i];
+        if (p.moving)
+        {
+            p.speed = moveTowards(p.speed, 0, deltaTime * guiData.piecesSpeedFriction);
+            p.rotationSpeed = moveTowards(p.rotationSpeed, 0, deltaTime * guiData.piecesRotationFriction);
+            
+            var translation = new THREE.Vector3().copy(p.velocity).multiplyScalar(p.speed * deltaTime);
+            
+            p.object3D.position.add(translation);
+            
+            translation.copy(p.object3D.position)
+            p.object3D.applyQuaternion(new THREE.Quaternion().setFromAxisAngle(p.rotationAxis, p.rotationSpeed * deltaTime));
+                
+            //if (i == 0) console.log(p.object3D.quaternion);
+        }
+    }
 }
 
 function restart() {
     animationTime = 0;
+    
+    for (let i = 0; i < pieces.length; ++i)
+    {
+        pieces[i].init();
+    }
 }
 
 canvas.addEventListener("mousedown", restart);
